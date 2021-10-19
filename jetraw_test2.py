21ececde2b93841b1be5c07dd583f1f11acedc3c
adamkglaser <adamkglaser@gmail.com>
2021-10-18 19:55:00 -0700

    Updated files

---
+++ jetraw_test2.py
@@ -0,0 +1,21 @@
+import pco
+import jetraw
+import time
+
+nFrames = 10
+
+cam = pco.Camera(camera_number = 2)
+cam.configuration = {'pixel rate': 272250000}
+print(cam.configuration)
+
+cam.record(nFrames, 'sequence dpcore')
+
+time.sleep(10)
+
+images, metas = cam.images()
+
+with jetraw.TiffWriter('C:/temp/recorder/img_jetraw.tif', description='PCO and Jetraw!') as tif:
+    for frame in images:
+        tif.write(frame)
+
+cam.close()
